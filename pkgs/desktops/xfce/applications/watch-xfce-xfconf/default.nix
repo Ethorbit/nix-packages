ade367710a3776ff95a7a81b1d9be11b410f2a39
Ethorbit <Ethorbit@protonmail.com>
2025-06-13 12:41:24 -0700

    Add metadata for watch-xfce-xfconf

--- pkgs/desktops/xfce/applications/watch-xfce-xfconf/default.nix
+++ pkgs/desktops/xfce/applications/watch-xfce-xfconf/default.nix
@@ -1,20 +1,33 @@
-{ python312Packages, fetchFromGitHub }:
+{
+    lib,
+    python312Packages,
+    fetchFromGitHub
+}:
 
 with python312Packages;
 
 buildPythonPackage rec {
     pname = "watch-xfce-xfconf";
     version = "1.0.5";
-    doCheck = false;
-    description = ''watch-xfce-xfconf: Configure XFCE 4 programmatically using the xfconf-query commands displayed by watch-xfce-xfconf'';
+
     src = (fetchFromGitHub {
         owner = "jamescherti";
         repo = "watch-xfce-xfconf";
         rev = "v${version}";
         hash = "sha256-CkKZ8ro2nZJ5h8Vxa9xvuMgiIgp6mWC4jIQWAX2xMO4=";
     });
+
     propagatedBuildInputs = [
         psutil
         lxml
     ];
+
+    doCheck = false;
+
+    meta = {
+        description = ''watch-xfce-xfconf: Configure XFCE 4 programmatically using the xfconf-query commands displayed by watch-xfce-xfconf'';
+        homepage = "https://github.com/jamescherti/watch-xfce-xfconf";
+        license = lib.licenses.mit;
+        platforms = lib.platforms.linux;
+    };
 }
