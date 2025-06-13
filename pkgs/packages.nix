60be269a547da46daad80c99addf7baee55a816f
Ethorbit <Ethorbit@protonmail.com>
2025-06-13 13:21:22 -0700

    Add steam-acolyte

--- pkgs/packages.nix
+++ pkgs/packages.nix
@@ -4,12 +4,14 @@ with pkgs;
 
 let
     cp = callPackage;
+    pycp = python3Packages.callPackage;
 in
 {
     mousejail = cp ./tools/X11/mousejail {};
     mount-wait = cp ./tools/misc/mount-wait {};
     mount-sshfs-run-service = cp ./tools/networking/mount-sshfs-run-service {};
     obs-studio-plugins.obs-pulseaudio-app-capture = cp ./applications/video/obs-studio/plugins/obs-pulseaudio-app-capture {};
+    steam-acolyte = pycp ./tools/games/steam-acolyte {};
     watch-xfce-xfconf = cp ./desktops/xfce/applications/watch-xfce-xfconf {};
     yt-dlp-wrapper = cp ./tools/misc/yt-dlp-wrapper {};
 }
---
+++ pkgs/tools/games/steam-acolyte/default.nix
@@ -0,0 +1,38 @@
+{
+    lib,
+    buildPythonPackage,
+    fetchFromGitHub,
+    pyqt5,
+    vdf,
+    docopt,
+    importlib-resources,
+    qt5
+}:
+
+buildPythonPackage rec {
+    pname = "steam-acolyte";
+    version = "0.10.0";
+
+    src = (fetchFromGitHub {
+        owner = "coldfix";
+        repo = "steam-acolyte";
+        rev = "v${version}";
+        hash = "sha256-ZVvuEvoXUmmUoOZJPLDVbQ7XvplP3ezhD6lhS0WfjiQ=";
+    });
+
+    propagatedBuildInputs = [
+        pyqt5
+        vdf
+        docopt
+        importlib-resources
+        qt5.full
+    ];
+
+    doCheck = false;
+
+    meta = {
+        description = ''Lightweight Steam Account Switcher'';
+        homepage = "https://github.com/coldfix/steam-acolyte";
+        license = lib.licenses.unlicense;
+    };
+}
