f818e537ac1bc9a0156bdb0356bfe3f2f75db965
Ethorbit <Ethorbit@protonmail.com>
2025-06-16 20:34:23 -0700

    Fix packages.nix line lengths

--- pkgs/packages.nix
+++ pkgs/packages.nix
@@ -10,7 +10,9 @@ rec {
     mount-wait = callPackage ./tools/misc/mount-wait {};
     mount-sshfs-run-service = callPackage ./tools/networking/mount-sshfs-run-service {};
 
-    obs-studio-plugins.obs-pulseaudio-app-capture = callPackage ./applications/video/obs-studio/plugins/obs-pulseaudio-app-capture {};
+    obs-studio-plugins.obs-pulseaudio-app-capture = (
+        callPackage ./applications/video/obs-studio/plugins/obs-pulseaudio-app-capture {}
+    );
 
     selkies-gstreamer = python311Packages.callPackage ./servers/selkies-gstreamer {
         ethorbit.pythonPackages = ethorbit.python311Packages;
@@ -18,7 +20,9 @@ rec {
 
     steam-acolyte = python312Packages.callPackage ./tools/games/steam-acolyte {};
 
-    watch-xfce-xfconf = python312Packages.callPackage ./desktops/xfce/applications/watch-xfce-xfconf {};
+    watch-xfce-xfconf = (
+        python312Packages.callPackage ./desktops/xfce/applications/watch-xfce-xfconf {}
+    );
 
     yt-dlp-wrapper = callPackage ./tools/misc/yt-dlp-wrapper {};
 }
