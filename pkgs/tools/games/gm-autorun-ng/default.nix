120694f5dbc461c598fc70fd5a478f7a3dc2b122
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2025-11-20 17:33:04 -0800

    Add desktop entry for Autorun-ng

--- pkgs/tools/games/gm-autorun-ng/default.nix
+++ pkgs/tools/games/gm-autorun-ng/default.nix
@@ -1,6 +1,8 @@
 {
     rustPlatform,
     fetchFromGitHub,
+    makeDesktopItem,
+    copyDesktopItems,
     lib
 }:
 
@@ -30,21 +32,35 @@ in
         postPatch = ''
             ln -s ${./Cargo.lock} Cargo.lock
 
-              echo "Patching all crate roots..."
-              for cargo in $(find . -name Cargo.toml); do
+            echo "Patching all crate roots..."
+            for cargo in $(find . -name Cargo.toml); do
                 dir=$(dirname "$cargo")
                 for root in "$dir"/src/lib.rs "$dir"/src/main.rs; do
-                  if [ -f "$root" ]; then
-                    echo "  -> Adding feature(let_chains) to $root"
-                    sed -i '1i #![feature(let_chains)]' "$root"
-                  fi
+                    if [ -f "$root" ]; then
+                        echo "  -> Adding feature(let_chains) to $root"
+                        sed -i '1i #![feature(let_chains)]' "$root"
+                    fi
                 done
-              done
+            done
         '';
 
         # Use nightly Rust
         RUSTC_BOOTSTRAP = "1";
         RUSTFLAGS = "--cfg=feature=\"let_chains\"";
+
+        desktopItems = [
+            (makeDesktopItem {
+                name = "autorun-ng";
+                desktopName = "Autorun NG";
+                comment = "GMod Lua execution environment";
+                exec = "autorun-egui";
+                icon = "autorun-ng";
+                categories = [ "Game" "Utility" ];
+                terminal = false;
+            })
+        ];
+
+        nativeBuildInputs = [ copyDesktopItems ];
 
         meta = with lib; {
             description = "A next-generation sandboxed GMod lua execution environment";
